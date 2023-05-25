53de4f5a84147163e65ce5175227e2c0d4f56ea4
Yukio Gabriel Miyashiro <yukio53@hotmail.com>
2023-05-25 10:59:34 -0300

    modulo D

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if(vel > 60) {
+    console.log(`Vc ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
